ed8f80ef67b0ace880b246403e409f1b7d24379a
Zac Braddy <zac.braddy@koodoo.io>
2019-01-22 20:58:34 +0000

    Introduced a simple oauth flow with no functionality as yet also added eslint and prettier to the project

--- server/index.js
+++ server/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
-const { PORT } = require('./env');
+const passport = require('passport');
+const { Strategy: GoogleStrategy } = require('passport-google-oauth20');
+const { SERVER_PORT } = require('./env');
+const { googleClientId, googleClientSecret } = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => res.send({ hi: 'there' }));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientId,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
 
-app.listen(PORT);
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.listen(SERVER_PORT);
+console.log('Listening of port: ', SERVER_PORT);
