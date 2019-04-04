5c9e896fecda85160c54b874a7be1b9740c42e82
Zac Braddy <zacharybraddy@gmail.com>
2019-04-04 21:46:20 +0100

    Added some stuff so it builds and works on heroku

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const path = require('path');
 const bodyParser = require('body-parser');
 const { SERVER_PORT } = require('./env');
 const { mongoUri, cookieKey } = require('./config/keys');
@@ -18,6 +19,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('__dirname', 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(SERVER_PORT);
 console.log('Listening on port: ', SERVER_PORT);
