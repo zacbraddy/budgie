54ac946a19dd88c435171bcf027f41583a4dec31
Zac Braddy <zacharybraddy@gmail.com>
2019-04-09 08:52:20 +0100

    Split the front end out from the back end in the project structure (#15)
    * Split client out from server
    
    * Made it so that we only install client deps if we aren't in production
    
    * Added back nodemon to server for development purposes and corrected
    mistake in the heroku post build task
    
    * Just checking what dirname is
    
    * Another attempt at fixing client being above server now
    
    * Change server port so that heroku populates it
    
    * Fixed pipeline now we've split out client from server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { SERVER_PORT } = require('./env');
+const { PORT } = require('./env');
 const { mongoUri, cookieKey } = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -21,12 +21,15 @@ authRoutes(app);
 billingRoutes(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve('__dirname', '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('__dirname', 'client', 'build', 'index.html'));
+    console.log('Serving request for fallback route: ', req.originalUrl);
+    res.sendFile(
+      path.resolve('__dirname', '..', 'client', 'build', 'index.html')
+    );
   });
 }
 
-app.listen(SERVER_PORT);
-console.log('Listening on port: ', SERVER_PORT);
+app.listen(PORT);
+console.log('Listening on port: ', PORT);
