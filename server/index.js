ab68e11c940d1a862bbc3b0da731be2c4eee0755
Zac Braddy <zacharybraddy@gmail.com>
2019-04-09 07:39:56 +0100

    Split client out from server

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ authRoutes(app);
 billingRoutes(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.resolve('__dirname', 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve('__dirname', 'client', 'build', 'index.html'));
