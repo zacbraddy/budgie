730af229a8880d12306a1d1ed4c37d2a728354a5
Zac Braddy <zacharybraddy@gmail.com>
2019-11-30 19:44:59 +0000

    Feature/clean up authn (#28)
    * INTERIM COMMIT starting bringing in firebase auth
    
    * Made it so you can log in and out of the app with firebase
    
    * Brought in react redux firebase to sort my shit out
    
    * Completed authentication
    
    * Updating node version in circleci

--- client/src/landing/index.js
+++ client/src/landing/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useFirebase } from 'react-redux-firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-const Landing = () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Budgie!</h1>
-    Straighten up ya' finances, Straighten up ya' finances! R'AWRK
-  </div>
-);
+const Landing = () => {
+  const firebase = useFirebase();
+  const history = useHistory();
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Budgie!</h1>
+      Straighten up ya' finances, Straighten up ya' finances! R'AWRK
+      <StyledFirebaseAuth
+        uiConfig={{
+          signInFlow: 'redirect',
+          signInSuccessUrl: '/budget',
+          signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+          /*callbacks: {
+            signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+              firebase.handleRedirectResult(authResult).then(() => {
+                console.log('zac', { authResult, redirectUrl });
+                history.push(redirectUrl);
+              });
+              return false;
+            },
+          },*/
+        }}
+        firebaseAuth={firebase.auth()}
+      />
+    </div>
+  );
+};
 
 export default Landing;
